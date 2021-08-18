7795b8b2e7a88c23f0a1bde2740f9a9f99adfb14
Layinka Jaji <layinkawebdev@gmail.com>
2021-08-18 15:34:34 +0100

    Initial project setup + HTML & CSS

---
+++ website/app.js
@@ -0,0 +1,20 @@
+const postData = async(url = '', data={}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+postData('/addGameTitle', {game: 'Assassins Creed'});
