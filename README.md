2ebb1edd5b5e27348e0757756becf913c44a3f23
Layinka Jaji <layinkawebdev@gmail.com>
2021-08-21 23:10:04 +0100

    Server and Client side code

---
+++ README.md
@@ -0,0 +1,28 @@
+# Udacity Frontend Web Developer Project 3
+
+Weather Journal App
+
+## Description
+In the words of Udacity, we are to "create an asynchronous web app that uses Web API and user data to dynamically update the UI".
+
+## Getting Started
+1. Clone this repo by using `git clone` command.
+2. Run `yarn install` to install all the dependencies of the project
+3. Start the server by running `node server.js` in the root of the project
+4. Edit in your favourite code editor
+5. Launch in your browser like so `localhost:3000`
+
+## Features
+1. Asynchronous JavaScript
+2. Web API
+3. Dynamically update UI
+
+
+## Technologies
+1. HTML
+2. CSS
+3. JavaScript
+
+
+## Author
+Me 😊
--- website/app.js
+++ website/app.js
@@ -1,5 +1,65 @@
-const postData = async(url = '', data={}) => {
-    console.log(data);
+// Get selectors
+const button = document.querySelector('button');
+const tempContent = document.querySelector('.temperature-text');
+const mainContent = document.querySelector('.main-text');
+const feelingContent = document.querySelector('.feeling-text');
+const dateContent = document.querySelector('.date-text');
+const cityContent = document.querySelector('.your-city-text');
+const detailsDiv = document.querySelector('.details');
+
+// Global variables
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = 'PLEASE REPLACE WITH YOUR OPENWEATHER API KEY';
+
+
+//Date
+const today = new Date();
+const theDate = `${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()}`;
+
+
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    const theCity = document.getElementById('city').value;
+    const yourFeelings = document.getElementById('feelings').value;
+    getTheWeatherData(baseURL, theCity, apiKey)
+        .then(function(data) {
+            //convert from Kelvin to Celsius
+            const toCelsius = (data.main.temp - 273.15)
+            console.log(toCelsius)
+            const temperature = Math.round(toCelsius);
+            console.log(temperature)
+            const mood = yourFeelings
+            const type = data.weather.map(param => param.main)
+            postData('/newEntry', {
+                temp: temperature,
+                date: theDate,
+                main: type,
+                feeling: mood,
+                thecity: theCity.toUpperCase()
+            })
+        })
+        .then(
+            () => updateUI()
+        )
+});
+
+
+// Get weather api details
+const getTheWeatherData = async (baseURL, city, key) => {
+    console.log(`${baseURL}${city}&appid=${key}`);
+    const response = await fetch(`${baseURL}${city}&appid=${key}`);
+    try {
+        const theWeatherData = await response.json();
+        return theWeatherData;
+    } catch (error) {
+        console.log('Data could not be fetched!', error);
+    }
+}
+
+
+// Boilerplate
+const postData = async(url = '', data) => {
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
@@ -10,11 +70,24 @@ const postData = async(url = '', data={}) => {
     });
     try {
         const newData = await response.json();
-        console.log(newData);
         return newData;
     } catch (error) {
-        console.log("error", error);
+        console.log("Something went wrong!", error);
     }
 }
 
-postData('/addGameTitle', {game: 'Assassins Creed'});
+// Update UI
+const updateUI = async () => {
+    const request = await fetch('/getAllData');
+    try {
+        const weatherData = await request.json();
+        detailsDiv.classList.add('entryHolder');
+        cityContent.textContent = weatherData.thecity;
+        dateContent.textContent = `Today\'s date: ${weatherData.date}`;
+        mainContent.textContent = `Type of Weather: ${weatherData.main}`;
+        tempContent.textContent = `The temperature is ${weatherData.temp}°C`;
+        feelingContent.textContent = `I\'m feeling ${weatherData.feeling}`;
+    } catch (error) {
+        console.log('The UI could not be updated', error);
+    }
+}
